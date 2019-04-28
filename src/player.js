2b081e49d5c15c6fb34acf69f551949e8e988f47
Ben Yu <intenex@gmail.com>
2019-04-28 20:40:08 +0900

    Add sleep function

--- src/player.js
+++ src/player.js
@@ -59,7 +59,45 @@ class Player {
     this.game.engine.unlock();
   }
 
+  _checkBox() {
+    const key = this._x + "," + this._y;
+    if (this.game.map[key] != "*") {
+      alert("There is no box here!");
+    } else if (key === this.game.ananas) {
+      alert("Hooray! You found an ananas and won this game.");
+      const keymap = [79, 80, 186, 191, 190, 188, 75, 73];
+      keymap.forEach(async function(val) { // a random clockwise victory dance lol
+        const e = { keyCode: val }; // emulate an event object lmao
+        this.game.engine.lock(); // just in case trying to unlock it when it's not locked causes some issue
+        await this._sleep(1000)
+        this.handleEvent(e);
+      });
+      this.game.engine.lock(); // so fucking amazing you can literally end the game by just locking the engine which puts a synchronous blocking call that prevents anything else from happening until the engine is unlocked again amazing and thus if you lock it at the end without doing anything else the game is effectively frozen and over so great
+      window.removeEventListener("keydown", this);
+    } else {
+      alert("This box is empty :-(");
+    }
+  }
 
+  /* remember promises and look into them way more so great
+     promises are async callback chains and either resolve or reject
+     await pauses the async until a promise resolves or rejects
+     look into async functions too ahhh yes these functions literally
+     mean one thing, which is that the function will always return a
+     promise, even if it actually returns a non-promise value, that will
+     be wrapped in a promise object look into how this works more
+     oh interesting it's like explicitly returning Promise.resolve(1)
+     if you return 1, it basically means whatever you return will be
+     passed as the argument to the resolve function (the .then that is
+     chained on. Remember that resolve and reject only take one argument
+     right? fucking love it. https://javascript.info/async-await
+     Man this shit is so fucking cool can't wait to just learn literally everything
+     note that await only pauses the current async function hmmm)
+     https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep */
+
+  _sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms)); // promises are created with a single callback that takes up to two arguments, resolve and reject, that specifies what to run when the promise is instantiated --> as soon as the Promise is created this function is run and the resolve function will consequently be triggered --> there is no chained .then so there is no resolve function here I believe but anyway this works great because of await which is way better than just chaining a .then to run the handleEvent because if you did that the code would keep running and there would just be a one second delay before everything happened not before each new action love this let's see if it works :)
+  }
 }
 
 export default Player;
