069ba635383d0e7d52665d0c351e54c70b70bf16
Ben Yu <intenex@gmail.com>
2019-04-30 09:18:13 +0900

    Add tile getter method

--- src/map.js
+++ src/map.js
@@ -1,6 +1,6 @@
 import * as TILES from './tile';
 
-// yeah okay this is actually pretty good design
+// yeah okay this is actually pretty good design love it
 
 class Map {
   constructor(tiles) {
@@ -17,5 +17,15 @@ class Map {
 
   getHeight() {
     return this.height;
+  }
+
+  // get the tile for a given coordinate set
+  getTile(x, y) {
+    // Make sure you're inside bounds. If not, return the null tile, right this Null Object pattern allows you to be able to treat all tiles the same even if they don't exist instead of separately having to do null checks all the time
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return TILES.nullTile;
+    } else {
+      return this.tiles[x][y] || TILES.nullTile; // if you're to do this why have a bounds check at all it'll just be undefined anyway sigh
+    }
   }
 }
