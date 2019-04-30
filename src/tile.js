c2a155e158149ad276884daedb039476c16a53e0
Ben Yu <intenex@gmail.com>
2019-04-30 09:04:24 +0900

    My favorite color goldenrod (creating tiles)

---
+++ src/tile.js
@@ -0,0 +1,20 @@
+import Glyph from './glyph';
+
+// right there should basically be singleton tiles awesome and you instantiate those just here? Interesting
+// right their design is to namespace everything to Game instead of having it be global
+// which is interesting and possibly the best JS could do in the past. Totally nuts though
+// So lucky to have ES6 now can't wait to keep building with it
+
+export class Tile {
+  constructor(glyph) {
+    this.glyph = glyph;
+  }
+
+  getGlyph() {
+    return this.glyph;
+  }
+}
+
+export const nullTile = new Tile(new Glyph());
+export const floorTile = new Tile(new Glyph('.'));
+export const wallTile = new Tile(new Glyph('#', 'goldenrod'));
