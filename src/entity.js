cb1da082b2c858cd020b6c9a4fffd89556676094
Ben Yu <intenex@gmail.com>
2019-05-01 22:06:22 +0900

    Fix glyph importation

--- src/entity.js
+++ src/entity.js
@@ -1,5 +1,5 @@
-import defaults from 'lodash/defaults'; // crazy syntax wow look into this more
-// lodash is god
+import defaults from 'lodash/defaults'; // // lodash is god // crazy syntax wow look into this more
+import Glyph from './glyph';
 
 // the basic prototype for everything in the game, from creatures to the player to items
 // consists of a glyph and a position and a name, the basic building blocks for representation
