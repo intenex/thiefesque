802ea6d69f9788eba3d245b7ffbfef5bbed448f1
Ben Yu <intenex@gmail.com>
2019-05-01 16:04:35 +0900

    Add setters and getters for Entity

--- src/entity.js
+++ src/entity.js
@@ -3,7 +3,34 @@
 
 class Entity extends Glyph {
   constructor(properties = {}) {
-    super(properties); 
+    super(properties);
+    this.name = properties.name || '';
+    this.x = properties.x || 0;
+    this.y = peroperties.y || 0;
   }
 
+  // setter for name dope
+  setName(name) {
+    this.name = name;
+  }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  setY(y) {
+    this.y = y;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
 }
