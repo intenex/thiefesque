c67f5944855299ac040b4187974595307d50c9c8
Ben Yu <intenex@gmail.com>
2019-05-01 19:16:04 +0900

    Add mixin functionality

--- src/entity.js
+++ src/entity.js
@@ -1,3 +1,4 @@
+import defaults from 'lodash/defaults'; // crazy syntax wow look into this more
 // lodash is god
 
 // the basic prototype for everything in the game, from creatures to the player to items
@@ -11,7 +12,15 @@ class Entity extends Glyph {
     this.y = properties.y || 0;
 
     // allow for some mixin functionality
-
+    this.attachedMixins = {};
+    const mixins = properties.mixins || []; // get all the mixins if any
+    mixins.forEach(function(mixin) {
+      defaults(this, mixin); // add all the properties of each mixin into the object at hand but use lodash's defaults which doesn't overwrite any existing properties which is a big no no here
+      this.attachedMixins[mixin.name] = true; // add the mixin
+      if (mixin.init) {
+        mixin.init.call(this, properties); // call is just an immediate bind (or a bind is an immediate call), you pass in the object the 'this' scope should be and then the arguments to pass in to the call function so awesome
+      }
+    }.bind(this)); // bind this anonymous function to the object at hand so you have access to its scope in assigning the mixins
   }
 
   // setter for name dope
