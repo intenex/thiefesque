a4dc7f679ecd9a4b28758d08df1a63e8708c76b8
Ben Yu <intenex@gmail.com>
2019-05-01 18:58:30 +0900

    Add lodash for the _.default method to use in creating mixins

--- src/entity.js
+++ src/entity.js
@@ -1,3 +1,5 @@
+// lodash is god
+
 // the basic prototype for everything in the game, from creatures to the player to items
 // consists of a glyph and a position and a name, the basic building blocks for representation
 
@@ -6,7 +8,10 @@ class Entity extends Glyph {
     super(properties);
     this.name = properties.name || '';
     this.x = properties.x || 0;
-    this.y = peroperties.y || 0;
+    this.y = properties.y || 0;
+
+    // allow for some mixin functionality
+
   }
 
   // setter for name dope
