870ac45e86769827ef7d37f8b9b7d7bea5fa4083
Ben Yu <intenex@gmail.com>
2019-05-01 15:57:46 +0900

    Have entities extend...glyphs o_O

--- src/entity.js
+++ src/entity.js
@@ -1,3 +1,9 @@
 // the basic prototype for everything in the game, from creatures to the player to items
 // consists of a glyph and a position and a name, the basic building blocks for representation
 
+class Entity extends Glyph {
+  constructor(properties) {
+    
+  }
+
+}
