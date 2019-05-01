b04ffbe2238493d34469794130459522cef4a9b1
Ben Yu <intenex@gmail.com>
2019-05-01 19:19:20 +0900

    Add hasMixin()

--- src/entity.js
+++ src/entity.js
@@ -16,9 +16,9 @@ class Entity extends Glyph {
     const mixins = properties.mixins || []; // get all the mixins if any
     mixins.forEach(function(mixin) {
       defaults(this, mixin); // add all the properties of each mixin into the object at hand but use lodash's defaults which doesn't overwrite any existing properties which is a big no no here
-      this.attachedMixins[mixin.name] = true; // add the mixin
+      this.attachedMixins[mixin.name] = true; // add the mixin to the list of all attached mixins so you can check for inclusion of a specific mixin later just by name or by passing in the object entirely with some nice metaprogramming magic
       if (mixin.init) {
-        mixin.init.call(this, properties); // call is just an immediate bind (or a bind is an immediate call), you pass in the object the 'this' scope should be and then the arguments to pass in to the call function so awesome
+        mixin.init.call(this, properties); // hopefully this has access to properties in scope let's find out otherwise just do a normal for loop or define this function elsewhere separately, almost certain it should have access though since the function is defined here in the right scope even if passed in as a callback later that's how closures should work // call is just an immediate bind (or a bind is an immediate call), you pass in the object the 'this' scope should be and then the arguments to pass in to the call function so awesome
       }
     }.bind(this)); // bind this anonymous function to the object at hand so you have access to its scope in assigning the mixins
   }
@@ -46,6 +46,15 @@ class Entity extends Glyph {
 
   getY() {
     return this.y;
+  }
+
+  hasMixin(obj) {
+    // check if obj or name as a string was passed in
+    if (typeof obj === 'object') {
+      return this.attachedMixins[obj.name]; // this is a bool that returns either true or undefined (aka false)
+    } else {
+      return this.attachedMixins[name]; // if typeof obj === 'string' since string is a primitive so dope
+    }
   }
 }
 
