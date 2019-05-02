db586801b508c0fad1df51271e027ce9916a7b19
Ben Yu <intenex@gmail.com>
2019-05-03 01:10:38 +0900

    Add appropriate mixins to entity templates

--- src/entities.js
+++ src/entities.js
@@ -6,13 +6,14 @@ Entities.PlayerTemplate = {
   character: '@',
   foreground: 'white',
   background: 'black',
-  mixins: [Mixins.Moveable, Mixins.PlayerActor]
+  mixins: [Mixins.Moveable, Mixins.PlayerActor,
+           Mixins.SimpleAttacker, Mixins.Destructible]
 };
 
 Entities.FungusTemplate = {
   character: 'F',
   foreground: 'green',
-  mixins: [Mixins.FungusActor]
+  mixins: [Mixins.FungusActor, Mixins.Destructible]
 };
 
 export default Entities; // do this when you're not exporting the class along with specific defined object instances of that class but rather just one collection of things perhaps
