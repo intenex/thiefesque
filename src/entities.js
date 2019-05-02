e58c53886e0c2986ff924a7cc0d0bd8dd0e289a4
Ben Yu <intenex@gmail.com>
2019-05-02 20:29:29 +0900

    Create fungus template

--- src/entities.js
+++ src/entities.js
@@ -9,6 +9,10 @@ Entities.PlayerTemplate = {
   mixins: [Mixins.Moveable, Mixins.PlayerActor]
 };
 
-
+Entities.FungusTemplate = {
+  character: 'F',
+  foreground: 'green',
+  mixins: [Mixins.FungusActor]
+};
 
 export default Entities; // do this when you're not exporting the class along with specific defined object instances of that class but rather just one collection of things perhaps
