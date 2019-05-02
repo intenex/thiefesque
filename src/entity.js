a8e1b39c656c285d7d1c2f4f7dcaf2329f40f4db
Ben Yu <intenex@gmail.com>
2019-05-02 16:55:46 +0900

    Add group mixin names

--- src/entity.js
+++ src/entity.js
@@ -13,10 +13,14 @@ export class Entity extends Glyph {
 
     // allow for some mixin functionality
     this.attachedMixins = {};
+    this.attachedMixinGroups = {}; // specifies having some general class of functionality like movement of some sort, regardless of the specific type of movement implemented
     const mixins = properties.mixins || []; // get all the mixins if any
     mixins.forEach(function(mixin) {
       defaults(this, mixin); // add all the properties of each mixin into the object at hand but use lodash's defaults which doesn't overwrite any existing properties which is a big no no here
       this.attachedMixins[mixin.name] = true; // add the mixin to the list of all attached mixins so you can check for inclusion of a specific mixin later just by name or by passing in the object entirely with some nice metaprogramming magic
+      if (mixin.groupName) { // if a mixin belongs to a group/class of similar mixins and has a name, add it here
+        this.attachedMixinGroups[mixin.groupName] = true;
+      }
       if (mixin.init) {
         mixin.init.call(this, properties); // hopefully this has access to properties in scope let's find out otherwise just do a normal for loop or define this function elsewhere separately, almost certain it should have access though since the function is defined here in the right scope even if passed in as a callback later that's how closures should work // call is just an immediate bind (or a bind is an immediate call), you pass in the object the 'this' scope should be and then the arguments to pass in to the call function so awesome
       }
