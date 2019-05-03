8a0f6f2878713c5c9456afa10a60c30ac5bcb8c3
Ben Yu <intenex@gmail.com>
2019-05-04 00:26:31 +0900

    Note that randomize doesn't exist and you'll need to build it

--- src/tile.js
+++ src/tile.js
@@ -19,6 +19,21 @@ export class Tile extends Glyph {
   isDiggable() {
     return this.diggable;
   }
+
+  getNeighborPositions(x, y) {
+    const tiles = [];
+    // generate all possible offsets
+    for (let dX = -1; dX < 2; dX ++) {
+      for (let dY = -1; dY < 2; dY++) {
+        // Make sure not the tile we're starting from
+        if (dX === 0 && dY === 0) {
+          continue; // fucking love this def keep learning all of these
+        }
+        tiles.push({x: x + dX, y: y + dY});
+      }
+    }
+    return tiles.randomize(); // this function does not exist you need to build it or find it in the ROT.JS library ideally under UTILS put UTILS in the window space and look through all its methods or something
+  }
 }
 
 export const nullTile = new Tile();
