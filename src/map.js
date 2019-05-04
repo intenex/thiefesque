0fdf00dd61b6d27deae0158c90d414bd4544ea02
Ben Yu <intenex@gmail.com>
2019-05-04 11:31:10 +0900

    Refactor entities to return an object with z keys and for each z key to point to an array of entities on that level z

--- src/map.js
+++ src/map.js
@@ -9,7 +9,7 @@ class Map {
     this.depth = tiles.length;
     this.width = tiles[0].length; // ah right all the columns makes sense dope a 2D array of tiles
     this.height = tiles[0][0].length; // a single column of all the rows
-    this.entities = []; // keep track of all entities on a given map in a list
+    this.entities = {}; // all the z levels are attributes on this object and point to an array of entities on each level // keep track of all entities on a given map in a list
     this.scheduler = new ROT.Scheduler.Simple();
     this.engine = new ROT.Engine(this.scheduler);
     this.addEntityAtRandomPosition(player);
@@ -36,10 +36,9 @@ class Map {
     // right this is such horribly inefficient look up time this should be refactored to be stored as an object with x, y coordinates as the lookup array key, a map object
     // would be ideal for that, ah but the problem is the coordinates change with time hmm maybe updating some hash map object with the new position of every entity on every move
     // would still function well but also there aren't that many entities ever that this is such an issue deal with it later
-    this.entities.forEach(entity => {
+    this.entities.z.forEach(entity => {
       if (entity.getX() === x &&
-          entity.getY() === y &&
-          entity.getZ() === z) {
+          entity.getY() === y) {
           return entity;
       }
     });
@@ -98,7 +97,12 @@ class Map {
     // set the entity's map
     entity.setMap(this);
     // Add the entity to this map's list of entities
-    this.entities.push(entity);
+    const z = entity.getZ();
+    if (this.entities.z) { // if the level has already been instantiated before it'll already be an array of entities and just push into it
+      this.entities.z.push(entity);
+    } else { // otherwise if this.entities.z is undefined then set that key to a new entity wrapped in an array
+      this.entities.z = [entity];
+    }
     // check if this entity is an actor, and if so add them to the scheduler
     if (entity.hasMixin('Actor')) {
       this.scheduler.add(entity, true); // true I believe readds them again after they act or something like that look into it again so they're not just one time
@@ -113,13 +117,15 @@ class Map {
   }
 
   removeEntity(entity) {
-    // Find entity in rhe list of entities if present
-    for (let i = 0; i < this.entities.length; i++) {
-      if (this.entities[i] === entity) {
-        this.entities.splice(i, 1); // remove 1 element starting from that index spot where the entity was found, i.e. remove that entity from the list of entities
+    // Find entity in the list of entities if present
+    // first get all the z levels as keys in the object array to iterate through
+    Object.keys(this.entities).forEach(z => {
+      // iterate through the whole array for each level until the entity is found --> ideally just refactor removeEntity to only search one level and to know what level to search from the beginning
+      for (let i = 0; i < this.entities.z.length; i++) {
+        this.entities.z.splice(i, 1); // remove 1 element starting from that index spot where the entity was found, i.e. remove that entity from the list of entities
         break; // end the for loop as soon as the entity is found if found
       }
-    }
+    });
     // If the entity is an actor, remove them from the scheduler, brilliant
     if (entity.hasMixin('Actor')) {
       this.scheduler.remove(entity);
@@ -143,12 +149,11 @@ class Map {
     // storing a reference of every entity at every X and Y position somewhere
     // entity finding by coordinates happens way too often such that this is almost
     // certainly worth optimizing at some point, esp if you have huge #s of entities
-    this.entities.forEach(entity => {
+    this.entities.currentZ.forEach(entity => {
       if (entity.getX() >= leftX &&
         entity.getX() <= rightX &&
         entity.getY() >= topY &&
-        entity.getY() <= bottomY &&
-        entity.getZ() === currentZ) {
+        entity.getY() <= bottomY) {
         boundedEntities.push(entity);
       }
     });
