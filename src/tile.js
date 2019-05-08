2de78e71565ea5d4f36d783baa2591f033502083
Ben Yu <intenex@gmail.com>
2019-05-08 13:37:01 +0900

    Update all tiles with the blocksLight property

--- src/tile.js
+++ src/tile.js
@@ -10,7 +10,7 @@ export class Tile extends Glyph {
     super(properties);
     this.walkable = properties.walkable || false;
     this.diggable = properties.diggable || false;
-    this.blocksLight = (properties.blocksLight !== undefined) ? properties.blocksLight : true; // love ternaries exact same structure here dope and dope that they do triple equals here lol you really should just learn triple and double equals appropriately in each case instead of just blindly always using triple equals
+    this.blocksLight = (properties.blocksLight !== undefined) ? properties.blocksLight : true; // either the property is defined as true or false in which case use that property or set it to true by default // love ternaries exact same structure here dope and dope that they do triple equals here lol you really should just learn triple and double equals appropriately in each case instead of just blindly always using triple equals
   }
 
   isWalkable() {
@@ -29,20 +29,24 @@ export class Tile extends Glyph {
 export const nullTile = new Tile();
 export const floorTile = new Tile({
   character: '.',
-  walkable: true
+  walkable: true,
+  blocksLight: false // nothing is magic everything works exactly as you define it in code so incredible and beautiful and so much abstracted away damn programming is so accessible these days it's so nuts so insanely grateful to get to do this or anything you really do have the best life in the world jesus fucking christ most of all to be able to appreciate it all
 });
 export const wallTile = new Tile({
   character: '#',
   foreground: 'goldenrod',
-  diggable: true
+  diggable: true,
+  blocksLight: true // might as well be explicit here no reason not to be to make things more clear
 });
 export const stairsUpTile = new Tile({
   character: '<',
   foreground: 'white',
-  walkable: true
+  walkable: true,
+  blocksLight: false
 });
 export const stairsDownTile = new Tile({
   character: '>',
   foreground: 'white',
-  walkable: true
+  walkable: true,
+  blocksLight: false
 });
