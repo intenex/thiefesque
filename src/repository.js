56b58ff844c05777a28fb91c1a19536ed36eca9a
Ben Yu <intenex@gmail.com>
2019-05-09 17:08:03 +0900

    Add notes on the Repository constructor

--- src/repository.js
+++ src/repository.js
@@ -1,3 +1,9 @@
+// awesome --> a repo has a name and a constructor
+// the constructor is the constructor function that the repository uses
+// to return a new object instance of that constructor function class
+// a repo then holds all the templates that can be passed to that constructor function
+// and can be used to easily get returned instantiated object instances of a given constructor function with any specified template or a random template
+
 class Repository {
   constructor(name, ctor) {
     this.name = name;
