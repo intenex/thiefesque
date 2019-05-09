9df4dfa2b3f434de0a354b458d70c026ee095967
Ben Yu <intenex@gmail.com>
2019-05-09 20:20:22 +0900

    add describeA lol

--- src/item.js
+++ src/item.js
@@ -9,4 +9,13 @@ export default class Item extends Glyph {
   describe() {
     return this.name;
   }
+
+  describeA(capitalize = false) {
+    const prefixes = capitalize ? ['A', 'An'] : ['a', 'an'];
+    const string = this.describe();
+    const firstLetter = string.charAt(0).toLowerCase();
+    // if word starts with a vowel, use an, otherwise use a
+    const prefix = 'aeiou'.indexOf(firstLetter) >= 0 ? 1 : 0; // will return -1 if index not found in string love it
+    return `${prefixes[prefix]} ${string}`;
+  }
 }
