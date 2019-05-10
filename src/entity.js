991debc24aa6efd432bb91950e1983502f282ffb
Ben Yu <intenex@gmail.com>
2019-05-10 15:54:28 +0900

    Fix one minor bug in importing Mixins into entity.js fuck yeah it worked

--- src/entity.js
+++ src/entity.js
@@ -1,6 +1,7 @@
 import defaults from 'lodash/defaults'; // // lodash is god // crazy syntax wow look into this more
 import * as TILES from './tile';
 import Glyph from './glyph';
+import { Mixins } from './entities';
 
 // the basic prototype for everything in the game, from creatures to the player to items
 // consists of a glyph and a position and a name, the basic building blocks for representation
