ee80242e9f14a3d4a4127e0d5f97b7878fb39b70
Ben Yu <intenex@gmail.com>
2019-05-11 10:27:17 +0900

    create apples and melons

--- src/items.js
+++ src/items.js
@@ -1,18 +1,24 @@
 import Item from './item';
 import Repository from './repository';
+import ItemMixins from './itemmixins';
 
 const ItemRepository = new Repository('items', Item);
 
 ItemRepository.define('apple', {
   name: 'apple',
   character: '%',
-  foreground: 'red'
+  foreground: 'red',
+  foodValue: 50,
+  mixins: [ItemMixins.Edible]
 });
 
-ItemRepository.define('rock', {
-  name: 'rock',
-  character: '*',
-  foreground: 'white'
+ItemRepository.define('melon', {
+  name: 'melon',
+  character: '%',
+  foreground: 'white',
+  foodValue: 35,
+  consumptions: 4,
+  mixins: [ItemMixins.Edible]
 });
 
 // right you can export default classes and functions directly but not variables those you have to do separately here make sure this is true look into it more
