8c837eeb7dfb7877557d2c9e539417ea529f58b4
Ben Yu <intenex@gmail.com>
2019-05-13 17:24:55 +0900

    refactor mixins.forEach to use an arrow function callback with fixed this in its scope vs manually binding a normal anonymous function lol

--- src/dynamicglyph.js
+++ src/dynamicglyph.js
@@ -13,16 +13,18 @@ export default class DynamicGlyph extends Glyph {
     this.listeners = {};
     this.init = {}; // just an empty object to make sure that this object doesn't get a mixin's init function written as its init function randomly when copying over defaults later on lol, otherwise useless not used
     const mixins = properties.mixins || []; // get all the mixins if any
-    mixins.forEach(function (mixin) {
+    mixins.forEach(mixin => {
       defaults(this, mixin); // add all the properties of each mixin into the object at hand but use lodash's defaults which doesn't overwrite any existing properties which is a big no no here
       this.attachedMixins[mixin.name] = true; // add the mixin to the list of all attached mixins so you can check for inclusion of a specific mixin later just by name or by passing in the object entirely with some nice metaprogramming magic
       if (mixin.groupName) { // if a mixin belongs to a group/class of similar mixins and has a name, add it here
         this.attachedMixinGroups[mixin.groupName] = true;
       }
+      // add all the listeners
+      if (mixin.listeners)
       if (mixin.init) {
         mixin.init.call(this, properties); // hopefully this has access to properties in scope let's find out otherwise just do a normal for loop or define this function elsewhere separately, almost certain it should have access though since the function is defined here in the right scope even if passed in as a callback later that's how closures should work // call is just an immediate bind (or a bind is an immediate call), you pass in the object the 'this' scope should be and then the arguments to pass in to the call function so awesome
       }
-    }.bind(this)); // bind this anonymous function to the object at hand so you have access to its scope in assigning the mixins
+    }); // instead of binding the function just use an arrow function lol
   }
 
   hasMixin(obj) {
